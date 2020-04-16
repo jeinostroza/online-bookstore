cb64cedda6a4ab81b84371b83e12c961d02074cb
jeinostroza <jeinostroza@hotmail.com>
2020-04-16 15:18:43 -0500

    Completed search by category

--- onlinebookstore/src/main/java/com/jeinostroza/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/jeinostroza/onlinebookstore/repository/BookRepository.java
@@ -1,6 +1,10 @@
 package com.jeinostroza.onlinebookstore.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.jeinostroza.onlinebookstore.entity.Book;
@@ -9,4 +13,6 @@ import com.jeinostroza.onlinebookstore.entity.Book;
 @CrossOrigin("*")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+	@RestResource(path="categoryid")
+	Page<Book> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
